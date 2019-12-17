e8cae5fefa5946ecf34d0be635d979197a48c427
Brent Maas <brent.maas@hotmail.com>
2019-12-17 13:45:17 +0100

    Day 17

---
+++ Day17/part2.py
@@ -0,0 +1,97 @@
+import io
+import sys
+import numpy as np
+import copy
+
+with open("Input.txt", "r") as f:
+	line = f.readline().split(",")
+	program = [int(i) for i in line]
+
+def getargs(program, p, modes, argc, relative_base):
+	args = []
+	for i in range(argc):
+		if len(modes) >= i + 1 and not modes[i] == 0:
+			if modes[i] == 1:
+				args.append(p + i + 1)
+			elif modes[i] == 2:
+				args.append(program[p + i + 1] + relative_base)
+		else:
+			args.append(program[p + i + 1])
+	return args
+
+def computer(program, noun=None, verb=None, stdin=sys.stdin, stdout=sys.stdout, returnonwrite=False, state=0, base_state=0):
+	if not noun is None:
+		program[1] = str(noun)
+	if not verb is None:
+		program[2] = str(verb)
+	p = state
+	relative_base = base_state
+	buffer = []
+	while True:
+		opcode = int(str(program[p])[-min(2, len(str(program[p]))):])
+		if len(str(program[p])) > 2:
+			modepart = str(program[p])[:len(str(program[p]))-2][::-1]
+			modes = [int(modepart[i]) for i in range(len(modepart))]
+		else:
+			modes = []
+		if opcode == 1: #Add
+			args = getargs(program, p, modes, 3, relative_base)
+			program[args[2]] = program[args[0]] + program[args[1]]
+			p += 4
+		elif opcode == 2: #Mult
+			args = getargs(program, p, modes, 3, relative_base)
+			program[args[2]] = program[args[0]] * program[args[1]]
+			p += 4
+		elif opcode == 3: #Read
+			while len(buffer) == 0:
+				inp = stdin.readline()
+				buffer += [int(i) for i in inp.split(" ")]
+			args = getargs(program, p, modes, 1, relative_base)
+			program[args[0]] = buffer[0]
+			buffer = buffer[1:]
+			p += 2
+		elif opcode == 4: #Write
+			args = getargs(program, p, modes, 1, relative_base)
+			print(program[args[0]], file=stdout)
+			p += 2
+			if returnonwrite:
+				return p, relative_base
+		elif opcode == 5: #Jump-if-true
+			args = getargs(program, p, modes, 2, relative_base)
+			if not program[args[0]] == 0:
+				p = program[args[1]]
+			else:
+				p += 3
+		elif opcode == 6: #Jump-if-false
+			args = getargs(program, p, modes, 2, relative_base)
+			if program[args[0]] == 0:
+				p = program[args[1]]
+			else:
+				p += 3
+		elif opcode == 7: #Less than
+			args = getargs(program, p, modes, 3, relative_base)
+			program[args[2]] = 1 if program[args[0]] < program[args[1]] else 0
+			p += 4
+		elif opcode == 8: #Equals
+			args = getargs(program, p, modes, 3, relative_base)
+			program[args[2]] = 1 if program[args[0]] == program[args[1]] else 0
+			p += 4
+		elif opcode == 9:
+			args = getargs(program, p, modes, 1, relative_base)
+			relative_base += program[args[0]]
+			p += 2
+		elif opcode == 99: #Exit
+			break
+		else:
+			raise RuntimeError(f"Invalid opcode: {opcode}")
+
+program[0] = 2
+
+m = "C,A,C,B,A,B,A,B,C,B\n"
+a = "L,8,R,10,R,6\n"
+b = "R,12,L,10,R,10,L,8\n"
+c = "R,12,L,10,R,12\n"
+stdin = io.StringIO(" ".join([str(ord(i)) for i in m]) + "\n" + " ".join([str(ord(i)) for i in a]) + "\n" + " ".join([str(ord(i)) for i in b]) + "\n" + " ".join([str(ord(i)) for i in c]) + "\n" + str(ord("n")) + " " + str(ord("\n")) + "\n")
+stdout = io.StringIO("")
+computer(program + [0] * 10000, stdin=stdin, stdout=stdout)
+print(stdout.getvalue().split("\n")[-2])
